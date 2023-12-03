a8915b4a98e78d770160b60147a8708a8e9210eb
nfjsg <140765984+nfjsg@users.noreply.github.com>
2023-12-03 18:42:49 -0500

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,1 +1,31 @@
+const express = require('express');
+const router = express.Router();
+
+// Middleware to check if the user is authenticated
+const isAuthenticated = (req, res, next) => {
+  if (req.session.userId) {
+    // User is authenticated, proceed to the next middleware or route
+    return next();
+  } else {
+    // User is not authenticated, redirect to login page or send an error response
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+};
+
+// Routes related to authentication
+router.get('/login', (req, res) => {
+  // Render the login page
+  res.render('login'); // Adjust this based on your view setup
+});
+
+router.get('/logout', (req, res) => {
+  // Destroy the session to log out the user
+  req.session.destroy(() => {
+    res.redirect('/'); // Redirect to the homepage or login page
+  });
+});
+
+// Add more authentication routes as needed
+
+module.exports = { router, isAuthenticated };
 
