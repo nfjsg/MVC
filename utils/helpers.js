a826fb7fa1fe7b6adcdf2da7e7e243d059950021
nfjsg <140765984+nfjsg@users.noreply.github.com>
2023-12-03 18:55:23 -0500

    Update helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,1 +1,15 @@
+const formatDate = (date) => {
+  // Format the date in a user-friendly way (e.g., "January 1, 2023")
+  return new Date(date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
+};
+
+const truncateText = (text, maxLength) => {
+  // Truncate text to a specified maximum length
+  if (text.length > maxLength) {
+    return text.substring(0, maxLength - 3) + '...';
+  }
+  return text;
+};
+
+module.exports = { formatDate, truncateText };
 
