831264708b13a8f9073382c2572d9fa25094aae2
nfjsg <140765984+nfjsg@users.noreply.github.com>
2023-12-03 18:44:48 -0500

    Update seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -1,1 +1,16 @@
+-- Insert sample users
+INSERT INTO users (username, password) VALUES
+  ('user1', 'hashedpassword1'),
+  ('user2', 'hashedpassword2');
+
+-- Insert sample posts
+INSERT INTO posts (title, content, userId) VALUES
+  ('First Post', 'This is the content of the first post.', 1),
+  ('Second Post', 'This is the content of the second post.', 2);
+
+-- Insert sample comments
+INSERT INTO comments (text, userId, postId) VALUES
+  ('Great post!', 2, 1),
+  ('Nice content!', 1, 2),
+  ('Interesting thoughts.', 2, 2);
 
