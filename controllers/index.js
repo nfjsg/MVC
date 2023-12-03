6b45248007c5332ee71bc7ac816d1ac5419bef3c
nfjsg <140765984+nfjsg@users.noreply.github.com>
2023-12-03 18:44:01 -0500

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const homeController = require('./homeController');
+const dashboardController = require('./dashboardController');
+const authController = require('./authController');
+const apiController = require('./api'); // Assuming you have an 'api' directory with related controllers
+
+// Use the controllers in your routes
+router.use('/', homeController);
+router.use('/', authController);
+router.use('/', dashboardController);
+router.use('/api', apiController); // Mount API routes under '/api'
+
+module.exports = router;
 
