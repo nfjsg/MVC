06bb1087e66f1fa6479bae34947414e2c8502f37
nfjsg <140765984+nfjsg@users.noreply.github.com>
2023-12-03 18:46:34 -0500

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,1 +1,31 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+// Define associations between models (if needed)
+User.hasMany(Post, {
+  foreignKey: 'userId',
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'userId',
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'userId',
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'userId',
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'postId',
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'postId',
+});
+
+module.exports = { User, Post, Comment };
 
