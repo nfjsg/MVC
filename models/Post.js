c00f6280fdca3b3ca6e386c5538f2f90f9475d9f
nfjsg <140765984+nfjsg@users.noreply.github.com>
2023-12-03 18:45:43 -0500

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -1,1 +1,41 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'users',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    modelName: 'post',
+  }
+);
+
+module.exports = Post;
 
